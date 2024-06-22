20289ce3f75d93b7c53f4bab62fbeff7bef43acd
Elias da Silva Rita <elias_rita@hotmail.com>
2024-06-22 20:25:48 -0300

    Add Solution template

--- source.extension.cs
+++ source.extension.cs
@@ -8,11 +8,11 @@ namespace BestPracticesCodeGenerator
     internal sealed partial class Vsix
     {
         public const string Id = "BestPracticesCodeGenerator.1bda164c-2fba-43e6-91bb-424ac2a31dac";
-        public const string Name = "BestPracticesCodeGenerator";
+        public const string Name = "BestPracticesGenerator";
         public const string Description = @"Generate clean architecture structure with Best.Practices framework";
         public const string Language = "en-US";
         public const string Version = "1.0";
-        public const string Author = "Elias da Silva Rita";
+        public const string Author = "Elias Silva";
         public const string Tags = "Clean architecture,DDD, OOP,Repository, Dapper";
     }
 }
